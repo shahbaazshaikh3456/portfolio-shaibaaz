01ed75f0433152888a6d8f340ae456fc4540386b
shahbaazshaikh3456 <117902121+shahbaazshaikh3456@users.noreply.github.com>
2024-08-11 15:24:20 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["App Developer","Data Analyst",],
+  strings : ["App Developer","Website Developer",],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
